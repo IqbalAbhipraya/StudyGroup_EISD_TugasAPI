6f165e5f469140e4ab868d772e67931b0d8ba8fe
IqbalAbhipraya <152391477+IqbalAbhipraya@users.noreply.github.com>
2025-09-18 21:21:08 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
     "dialect": "postgres"
   }
 }
